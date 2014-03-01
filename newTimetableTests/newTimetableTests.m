105511fc128c5b16018c406127d4bf307eb1f49c
codeofRobin <malhotra.robin@icloud.com>
2014-03-01 13:32:34 +0530

    Initial Commit

---
+++ newTimetableTests/newTimetableTests.m
@@ -0,0 +1,34 @@
+//
+//  newTimetableTests.m
+//  newTimetableTests
+//
+//  Created by Robin Malhotra on 01/03/14.
+//  Copyright (c) 2014 Robin's code kitchen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface newTimetableTests : XCTestCase
+
+@end
+
+@implementation newTimetableTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
